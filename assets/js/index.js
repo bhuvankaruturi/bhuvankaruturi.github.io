40f3598481a46c0f1e88d783d09a64be003b55ce
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2020-06-05 12:18:37 -0500

    added service worker for caching

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
+// adding service workers for offline loading
+// check if the browser supports service workers
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+                .register('/sw_cache_site.js')
+                .then(reg => console.log("Service worker registered"))
+                .catch(err => console.error("Service worker registration error"))
+    })
+}
+
+// function to update the progress bar at the top of the page
 window.onscroll = function() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -5,6 +17,8 @@ window.onscroll = function() {
     document.getElementById("progress-bar").style.width = scrolled + "%";
 }
 
+// observers for each section to add animations when
+// the particular sections enters or leaves the view port
 const setObservers = function() {
     this.observers = [];
     let options = {
---
+++ sw_cache_site.js
@@ -0,0 +1,48 @@
+// cache name
+const cachename = "bpkc1"
+
+// installing the service worker
+self.addEventListener('install', e => {
+    console.log("Service worker installed");
+})
+
+// activating the service worker
+self.addEventListener('activate', e => {
+    console.log("Service worker activated");
+    // delete old caches
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cachename) {
+                        console.log('Service Worker: Clearing old cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// cache the response on a fetch event
+self.addEventListener('fetch', e => {
+    console.log('Service worker: fetching');
+    e.respondWith(
+        // browser request to load the page
+        fetch(e.request)
+            .then(res => {
+                // make a copy of the response
+                const resCopy = res.clone();
+                // open cache
+                caches
+                    .open(cachename)
+                    .then(cache => {
+                        // Add the response copy to cache
+                        console.log(cache); 
+                        cache.put(e.request, resCopy);
+                    });
+                return res;
+            })
+            .catch(err => caches.match(e.request).then(res => res))
+    )
+})
