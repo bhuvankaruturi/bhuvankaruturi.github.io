756fa927dd79d606945f0e535a1628895b9dcebc
Bhuvan Karuturi <bhuvankaruturi123@gmail.com>
2021-12-12 21:26:54 -0600

    data files to load career events

---
+++ assets/data/experience.ts
@@ -0,0 +1,68 @@
+export default {
+    "id": "experience",
+    "header": "Work Experience",
+    "subsections": [
+        {
+            "header": "Amazon.com Services LLC",
+            "logo": "assets/images/amazon-logo.jpg",
+            "location": "Seattle, Washington",
+            "subtitle": "Software Development Engineer Intern",
+            "duration": "May 2021 - August 2021",
+            "details": [
+                "Developed an ML-based system to classify fraudulent transactions based on features derived from 4 different systems.",
+                "Gained experience of working with terabyte-scale data in various data stores such as RedShift, DynamoDB.",
+                "Trained an ML model which demonstrated the ability to reduce the labor costs for manual review by 73%."
+            ]
+        },
+        {
+            "header": "Ohio-At-Home Healthcare Agency",
+            "logo": "assets/images/ohio-at-home-logo.jpg",
+            "location": "Columbus, Ohio",
+            "subtitle": "Full Stack Web Developer Intern",
+            "duration": "August 2020 - May 2021",
+            "details": [
+                "Developed a full stack web app from scratch with ASP.NET core, XUnit, Angular, Docker and Azure Kubernetes Service.",
+                "Used microservices architecture to achieve service decoupling and faster updates deployment. Used the Kubernetes engine to manage microservices. Deployed an Nginx Ingress Service to balance the load and enable TLS termination.",
+                "Developed 6 microservices to process business logic and to report Kubernetes cluster health status. Used asynchronous communication between services using Azure Event Bus message queues.",
+                "Developed an API gateway using ASP.NET core to route incoming traffic to a microservice. Implemented Azure AD based authentication and authorization on the API Gateway and the Angular client.",
+                "Developed a responsive SPA with Angular 10 and Material design. Deployed the containerized app on Azure App service."
+            ]
+        },
+        {
+            "header": "The University of Texas at Dallas",
+            "logo": "assets/images/utd-logo.jpg",
+            "location": "Dallas, Texas",
+            "subtitle": "Teaching Assistant",
+            "duration": "January 2020 - May 2020",
+            "details": [
+                "Teaching assistant for the senior year operating systems course. Tutored students on general OS and Linux concepts.",
+                "Developed programming assignments and automated test scripts. Implemented research papers in C."
+            ]
+        },
+        {
+            "header": "UT Dallas - Center for Education Outreach",
+            "logo": "assets/images/utd-logo.jpg",
+            "location": "Dallas, Texas",
+            "subtitle": "Coding Instructor",
+            "duration": "August 2019 - November 2019",
+            "details": [
+                "Taught machine learning and data science concepts such as data preprocessing, data visualization, Image augmentation, Image classification, Transfer Learning using Keras library.",
+                "Developed Machine learning algorithm along with the student for the Kaggle competition, APTOS 2019 Blindness Detection."
+            ]
+        },
+        {
+            "header": "Capgemini Technology Services",
+            "logo": "assets/images/capgemini-logo.jpg",
+            "location": "Mumbai, Maharashtra",
+            "subtitle": "Senior Software Engineer",
+            "duration": "May 2017 - May 2019",
+            "details": [
+                "Developed customizations to ERP module using Java, SQL, PL/SQL. Built robust and modular packages that received faster user acceptance.",
+                "Gathered client requirements for the project, performed feasibility studies, and developed technical design documents.",
+                "Researched and developed a browser-based package migration automation tool using JavaScript, HTML, CSS and batch programming and achieved a 70% reduction in package migration time.",
+                "Developed Python automation scripts for package reorganization tasks which eliminated human errors and significantly reduced the time required for package reorganization.",
+                "Received the project STAR award in 2018 for prompt and efficient solutions delivered as part of the engagement."
+            ]
+        }
+    ]
+}
